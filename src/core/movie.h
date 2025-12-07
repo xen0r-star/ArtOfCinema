0a1c4e58c33696f9c3fdf17742552071c42a3e3e
Florian <98989004+xen0r-star@users.noreply.github.com>
2025-12-07 15:16:28 +0100

    :sparkles: Refactor movie loading logic and update data structure for improved functionality

--- src/core/movie.h
+++ src/core/movie.h
@@ -2,7 +2,12 @@
 #define MOVIE_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "../types/movie.h"
 
+
+int loadMovies();
+MovieNode* getMovieList();
 
 #endif
--- src/types/movie.h
+++ src/types/movie.h
@@ -1,27 +1,15 @@
 #ifndef TYPE_MOVIE_H
 #define TYPE_MOVIE_H
 
-typedef enum Type {
-    TYPE_NONE,
-    TYPE_ACTION,
-    TYPE_COMEDY,
-    TYPE_DRAMA,
-    TYPE_HORROR,
-    TYPE_SCI_FI,
-    TYPE_ROMANCE,
-    TYPE_THRILLER,
-    TYPE_DOCUMENTARY,
-    TYPE_ANIMATION,
-} Type;
 
 typedef struct Movie {
     int id;
     char *name;
     int duration;
-    Type *types;
+    char *types;
     int type_count;
 
-    float note;
+    int note;
     char *description;
     int year;
     char *director;
@@ -31,5 +19,10 @@ typedef struct Movie {
     char **cast;
     int cast_count;
 } Movie;
+
+typedef struct MovieNode {
+    Movie movie;
+    struct MovieNode *next;
+} MovieNode;
 
 #endif
